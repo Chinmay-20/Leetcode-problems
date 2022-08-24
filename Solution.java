0503b2a8969144f02ec6a6585a8d886422fdaf35
Chinmay Kulkarni <59417006+Chinmay-20@users.noreply.github.com>
2022-08-25 05:02:48 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,18 +1,1 @@
-//recursive approach to find fibonaaci number
-class Solution 
-{
-    
-    public int fib(int n) 
-    {
-        if(n<=1)
-        {
-            return n;
-        }
-        return fib(n-2)+fib(n-1);
-    }
-    public static void main(String args[])
-    {
-    	//System.out.println(fib(2));
-    	System.out.println(fib(3));
-    }
-}
+
